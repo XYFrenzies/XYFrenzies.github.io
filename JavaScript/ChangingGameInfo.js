5c6afe72ff7a52dde190a38ebd840bedb1d9a211
Ben <bennybill16@gmail.com>
2021-10-20 11:59:15 +1100

    Finished updating the javaScript file.

--- JavaScript/ChangingGameInfo.js
+++ JavaScript/ChangingGameInfo.js
@@ -1,5 +1,32 @@
 
 var slideIndex = 1;
+var i;
+function ChangeSlides(n)
+{
+    var x = document.getElementsByClassName("GameImages");
+    var dots = document.getElementsByClassName("SlideCurrent");
+    n++;
+    if(n > x.length)
+    {
+        slideIndex = 1;
+    }
+    else if(n < 1)
+    {
+        slideIndex = x.length;
+    }
+    for(i = 0; i < x.length; i++)
+    {
+        x[i].style.display = "none";
+    }
+    for(i = 0; i < dots.length; i++)
+    {
+        dots[i].className = dots[i].className.replace("white", "");
+    }
+
+    x[slideIndex-1].style.display = "block";  
+    dots[slideIndex-1].className += "white";
+    setTimeout(carousel, 2500); 
+}
 function IncrementSlides(n)
 {
     ChangeSlides(slideIndex += n);
@@ -13,30 +40,6 @@ function DecrementSlides(n)
 function CurrentSlide(n)
 {
     ChangeSlides(slideIndex = n)
-}
-
-function ChangeSlides(n)
-{
-    var i;
-    var x = document.getElementsByClassName("GameImages");
-    var dots = document.getElementsByClassName("SlideCurrent");
-    if(n > x.length)
-    {
-        slideIndex = 1;
-    }
-    else if(n < 1)
-    {
-        slideIndex = x.length;
-    }
-    
-    for(i = 0; i < x.length; i++)
-    {
-        x[i].style.display = "none";
-    }
-    for(i = 0; i < dots.length; i++)
-    {
-        
-    }
 }
 function main()
 {
