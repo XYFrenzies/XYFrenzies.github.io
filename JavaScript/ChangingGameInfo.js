861054123493016da357a0a9f822a6578f49809e
Ben <bennybill16@gmail.com>
2021-10-20 11:49:49 +1100

    Expanding upon the javascript.

--- JavaScript/ChangingGameInfo.js
+++ JavaScript/ChangingGameInfo.js
@@ -1,16 +1,44 @@
 
 var slideIndex = 1;
-function IncrementSlides()
+function IncrementSlides(n)
 {
-    
+    ChangeSlides(slideIndex += n);
 }
 
-function DecrementSlides()
+function DecrementSlides(n)
 {
-    
+    ChangeSlides(slideIndex -= n);
 }
 
+function CurrentSlide(n)
+{
+    ChangeSlides(slideIndex = n)
+}
+
+function ChangeSlides(n)
+{
+    var i;
+    var x = document.getElementsByClassName("GameImages");
+    var dots = document.getElementsByClassName("SlideCurrent");
+    if(n > x.length)
+    {
+        slideIndex = 1;
+    }
+    else if(n < 1)
+    {
+        slideIndex = x.length;
+    }
+    
+    for(i = 0; i < x.length; i++)
+    {
+        x[i].style.display = "none";
+    }
+    for(i = 0; i < dots.length; i++)
+    {
+        
+    }
+}
 function main()
 {
-    
+    ChangeSlides(slideIndex);
 }
